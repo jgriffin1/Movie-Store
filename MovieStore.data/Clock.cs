88a86d445042ed222f19f6cbacf8f3dfc1d05021
Josh Griffin <jgriffin1@umary.edu>
2021-01-21 16:57:38 -0600

    Database Created

---
+++ MovieStore.data/Clock.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieStore.data
+{
+    public class Clock
+    {
+        public int Hr { get; set; }
+        public int Min { get; set; }
+        public int Sec { get; set; }
+
+        //default constructor
+        public Clock()
+        {
+
+        }
+        public Clock(int hr, int min, int sec)
+        {
+            this.Hr = hr;
+            this.Min = min;
+            this.Sec = sec;
+        }
+
+    }
+}
